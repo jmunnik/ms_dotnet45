6c59e0fdca10c7d4808da785b4e1f3f443b0368a
Jos Munnik <jmunnik@schubergphilis.com>
2015-02-27 14:26:47 +0100

    refactored check .NET Framework

--- recipes/default.rb
+++ recipes/default.rb
@@ -23,25 +23,29 @@ if platform?('windows')
   if win_version.windows_server_2008? || win_version.windows_server_2008_r2? || win_version.windows_7? || win_version.windows_vista? || windows_version.windows_server_2012? || windows_version.windows_server_2012_r2?
      # Determine which release of .NET Framework has been installed (4.5, 4.5.1 or 4.5.2). Normally this can already been done via windows package but 
      # because 4.5 is embedded in windows 2012 R2 it's not in the Installed Programs.
-     unless registry_data_exists?('HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full',
-         { 
-		:name => "Release", 
-	 	:type => :dword, 
-		:data => node['ms_dotnet45']['release'] 
-	  },	:x86_64
-      )
-      
+     registry_path = registry_get_values('HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full')
+    
+     dotnet_release = nil
+     registry_path.each do | registry_key |
+	if registry_key[:name] == 'Release'  
+          dotnet_release = registry_key[:data]
+        end
+     end
+
+	
+    if "#{dotnet_release}" == "#{node['ms_dotnet45']['release']}"
+       Chef::Log.info("Microsoft .NET Framework #{node['ms_dotnet45']['version']} is already installed")
+     else 
       # Call windows_package and version as displayed in Installed Programs 
       windows_package "Microsoft .NET Framework #{node['ms_dotnet45']['version']}" do 
         source node['ms_dotnet45']['http_url']
         installer_type :custom
         options '/quiet /norestart'
         timeout node['ms_dotnet45']['timeout']
-	action :install
         # If a previous version of .NET Framework is installed (for example 4.0) an removal will take place. After installation of 4.5.x you will receive
         # ==> Mixlib::ShellOut::ShellCommandFailed. Expected process to exit with [0, 42, 127], but received '3010' which means; 
-        # A reboot is required but not really necesarry
-         success_codes [0, 3010]
+        # A reboot is required but not really necesarry, but it's maybe better to reboot
+        success_codes [0, 3010]
       end
     end
   elsif win_version.windows_server_2003_r2? || win_version.windows_server_2003? || win_version.windows_xp?
