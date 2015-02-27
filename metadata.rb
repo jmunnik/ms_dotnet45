a889415052d21519eaad46f5d3bc376ff9aacded
Jos Munnik <jmunnik@schubergphilis.com>
2015-02-27 09:07:08 +0100

    Upped version to 2.0.1 /metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,6 @@ license           'Apache 2.0'
 license           'All rights reserved'
 description       'Installs Microsoft .NET 4.5'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           '2.0.0'
+version           '2.0.1'
 supports          'windows'
 depends           'windows'
